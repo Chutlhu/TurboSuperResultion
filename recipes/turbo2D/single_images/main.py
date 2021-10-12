aa34666cb6406100e99dc5e2fc9c76e428579447
Chutlhu <diego.di-carlo@univ-rennes2.fr>
2021-10-12 16:33:38 +0200

    preparing pl

---
+++ recipes/turbo2D/single_images/main.py
@@ -0,0 +1,58 @@
+import argparse
+
+import os
+import numpy as np
+
+# DNN Libraries
+import torch
+# import pl torch libs
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+
+# Turboflow Libraries
+from turboflow.dataloaders import Turbo2DDataModule
+from turboflow.models.phyrff_hard import plDivFreeRFFNet
+
+def get_path_and_prepare_folder():
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    return root_dir
+
+def get_args():
+
+    # these are project-wide arguments
+    parser = argparse.ArgumentParser(prog='main.py', add_help=True)
+    parser.add_argument('--fast_dev_run',
+                               default=False, action='store_true',
+                               help='fast_dev_run: runs 1 batch of train, test, val (ie: a unit test)')
+    # add data specific arguments
+    parser = Turbo2DDataModule.add_data_specific_args(parser)
+    # add model specific arguments
+    parser = plDivFreeRFFNet.add_model_specific_args(parser)
+    # add all the available trainer options to argparse
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    
+    hparams = get_args()
+
+    # PREPARE FOLDER
+    root_dir = get_path_and_prepare_folder()
+
+    # # DATA
+    dm = Turbo2DDataModule(hparams)
+
+    # MODEL
+    model = plDivFreeRFFNet(hparams)
+
+    early_stop_callback = EarlyStopping(monitor='val_loss')
+    trainer = pl.Trainer(gpus=1, 
+                     log_every_n_steps=1,
+                     check_val_every_n_epoch=200, 
+                     max_epochs=5000, 
+                     callbacks=[early_stop_callback])
+
+    # TRAIN    
+    trainer.fit(model, dm)
+    # TEST
+    # trainer.test()
---
+++ recipes/turbo2D/single_images/run.sh
@@ -0,0 +1,4 @@
+
+data_dir=/home/dicarlo_d/Documents/Code/TurboSuperResultion/data/2021-Turb2D_velocities.npy
+
+python main.py --data_dir $data_dir --fast_dev_run true
